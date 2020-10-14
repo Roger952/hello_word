9ffe8cfbb25cd930ab628e2850bd7de4309468b0
Roger Joel <roger.guillermo@hbsis.com.br>
2020-10-14 18:40:51 -0300

    Meu primeiro app finalizado

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +9,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _resultado = "resultado";
+
+  TextEditingController _controllerCep = TextEditingController();
+
+  _recuperarCep() async {
+    String cep = _controllerCep.text;
+    String url = "https://viacep.com.br/ws/${cep}/json/";
+    http.Response response;
+
+    response = await http.get(url);
+    Map<String, dynamic> retorno = jsonDecode(response.body);
+
+    String logradouro = retorno["logradouro"];
+    String complemento = retorno["complemento"];
+    String bairro = retorno["bairro"];
+    String localidade = retorno["localidade"];
+
+    setState(() {
+      _resultado = "${logradouro}, ${complemento}, ${bairro}, ${localidade}";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,16 +44,17 @@ class _HomeState extends State<Home> {
             RaisedButton(
               child: Text("Clique aqui"),
               onPressed: _recuperarCep,
+            ),
+            Text(_resultado),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: "Digite o cep ex: 05428200"),
+              style: TextStyle(fontSize: 20),
+              controller: _controllerCep,
             )
           ],
         ),
       ),
     );
-  }
-
-  _recuperarCep() {
-
-    String cep = "01311300";
-    String url = "https://viacep.com.br/ws/${cep}/json/";
   }
 }
